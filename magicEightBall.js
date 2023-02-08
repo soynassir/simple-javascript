4adca67fb856b5d4bac7c054d202937e0a43d7ad
Nassir ∞ <118899988+soynassir@users.noreply.github.com>
2023-02-08 09:09:56 +0000

    Create magicEightBall.js

---
+++ magicEightBall.js
@@ -0,0 +1,45 @@
+let userName = 'Nassir';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+const userQuestion = 'Will I get a job?';
+
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'It is certain';
+    break;
+  case 7:
+    eightBall = 'Outlook not so good';
+    break;
+  case 8:
+    eightBall = 'Signs point to yes';
+    break;
+  default:
+    eightBall = '';
+    break;
+}
+
+console.log(eightBall)
