e7ba724aa2afde0a5a7272ad89817717564b7950
Nassir ∞ <118899988+soynassir@users.noreply.github.com>
2023-02-19 08:58:20 +0000

    Update and rename number-datatype to number-datatype.js

--- number-datatype.js
+++ number-datatype.js
@@ -13,3 +13,16 @@ let y = 123e-5;
 console.log(x); // -> 9000
 console.log(y); // -> 0.00123
 console.log(typeof y);
+let a = 1 / 0;
+let b = -Infinity;
+
+console.log(a); // -> Infinity
+console.log(b); // -> -Infinity
+console.log(typeof a); // -> number
+console.log(typeof b); // -> number
+
+let s = "it's definitely not a number";
+let n = s * 10;
+console.log(n); // -> NaN
+console.log(typeof n); // -> number
+
